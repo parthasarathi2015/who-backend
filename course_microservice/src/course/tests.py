3eb0dc44978c19d520526e37d460d2edb6bd32d7
parthasarathi2015 <paromoni2008@gmail.com>
2023-04-03 12:19:18 +0530

    fit(test): add test in  pytest

--- course_microservice/src/course/tests.py
+++ course_microservice/src/course/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from unittest import mock
+from config import settings
 
-# Create your tests here.
+client = APIClient()
+
+@pytest.mark.django_db
+class TestCourseListView:
+
+    def test_course_list_view_with_authentication(self):
+        url = reverse('course-list')
+        client.credentials(HTTP_AUTHORIZATION=f'Token {settings.CUSTOM_TOKEN}')
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_course_list_view_without_authentication(self):
+        url = reverse('course-list')
+        response = client.get(url)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.django_db
+class TestAuthenticateUserView:
+
+    def test_authenticate_user_view_with_valid_token(self):
+        url = reverse('authenticate-user')
+        client.credentials(HTTP_AUTHORIZATION=f'Token {settings.CUSTOM_TOKEN}')
+        response = client.get(url)
+        assert response.data == True
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_authenticate_user_view_with_invalid_token(self):
+        url = reverse('authenticate-user')
+        client.credentials(HTTP_AUTHORIZATION='Token abcdefghijklmnopqrstuvwxyz')
+        response = client.get(url)
+        assert response.data == False
+        assert response.status_code == status.HTTP_200_OK
+
